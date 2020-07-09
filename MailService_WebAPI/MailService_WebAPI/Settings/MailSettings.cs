dd129fd5deee962c68d7a913fdda053ab36990f4
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-07-09 12:13:27 +0200

    Add class properties

--- MailService_WebAPI/MailService_WebAPI/Settings/MailSettings.cs
+++ MailService_WebAPI/MailService_WebAPI/Settings/MailSettings.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MailService_WebAPI.Settings
+﻿namespace MailService_WebAPI.Settings
 {
     public class MailSettings
     {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
     }
 }
